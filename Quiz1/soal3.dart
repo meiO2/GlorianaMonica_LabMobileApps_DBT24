8401c2f8c85a58e12a69e0f885bc45a34709414b
Gloriana Monica <glorianamonica06@gmail.com>
2026-01-21 15:29:39 +0700

    soal3

--- Quiz1/soal3.dart
+++ Quiz1/soal3.dart
@@ -3,19 +3,20 @@ import 'dart:math';
 
 void main() {
   final random = Random();
-  int randNumber = random.nextInt(3);
+  int randNumber = random.nextInt(100)+1;
   int percobaan = 0;
 
   stdout.write("Masukkan angka tebakan:");
-  int input = stdin.readByteSync();
-  while (randNumber != input) {
+  int input = int.parse(stdin.readLineSync()!);
+  while (input != randNumber) {
     if (input < randNumber) {
       print ("tebakan terlalu kecil");
-      percobaan += 1;
-    } else if (input > randNumber) {
+    } else {
       print ("tebakan terlalu besar");
-      percobaan += 1;
     }
+    percobaan += 1;
+    stdout.write("Masukkan angka tebakan:");
+    input = int.parse(stdin.readLineSync()!);
   }
   print ("Selamat, berhasil menebak setelah $percobaan percobaan");
 }

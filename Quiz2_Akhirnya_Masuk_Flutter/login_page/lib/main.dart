e809a0dd08e99a0d616ce266c8400f8a772b7de0
Gloriana Monica <glorianamonica06@gmail.com>
2026-01-28 14:13:37 +0700

    push to repo quizlab week4

---
+++ Quiz2_Akhirnya_Masuk_Flutter/login_page/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 65, 106, 228)),
+      ),
+      home: const MyHomePage(title: 'Login Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Selamat Datang',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            const ObscuredTextFieldSample(),
+            const SizedBox(height: 12),
+            const ObscuredTextFieldSample2(),
+            const SizedBox(height: 20),
+            const Text('Created by ERUMA', style: TextStyle(fontSize: 10)),
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class ObscuredTextFieldSample extends StatelessWidget {
+  const ObscuredTextFieldSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Email'),
+      ),
+    );
+  }
+}
+
+class ObscuredTextFieldSample2 extends StatelessWidget {
+  const ObscuredTextFieldSample2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Password'),
+      ),
+    );
+  }
+}
